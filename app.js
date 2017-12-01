d251753d593c82a193eb34e016ac13a9bb6f0f57
Randi Mortensen <3884187@rts-365.dk>
2017-12-01 13:03:44 +0100

    port update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const router = require('./router');
 
-http.createServer(router).listen(3003);
-console.log('Server startet. Port: 3003');
+http.createServer(router).listen(5000);
+console.log('Server startet. Port: 5000');

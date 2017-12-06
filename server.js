4addcfde02dd7723dbee85055e54d7e2875f5202
Randi Mortensen <31003445+Randi-Mortensen@users.noreply.github.com>
2017-12-06 10:34:22 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
+const restify = require('restify');
+const path = require('path');
 const http = require('http');
 const router = require('./router');
 
-http.createServer(router).listen(3003);
-console.log('Server startet. Port: 3003'); 
+const port = process.env.port || 3003;
+
+const app = restify.createServer({
+    name: 'webserverTest',
+    version: '1.0.0'
+})
+
+require(path.join(__dirname, 'routes', 'index'))(app);
+
+
+app.listen(port, function (err) {
+    console.log(err);
+    console.log('%s is listening on %s', app.name, port);
+})
+
+// const http = require('http');
+// const router = require('./router');
+
+// http.createServer(router).listen(3003);
+// console.log('Server startet. Port: 3003');
